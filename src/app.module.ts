c0561198a53471bf275282ac9b5102a842d404ab
cya-nyde <134979128+cya-nyde@users.noreply.github.com>
2025-05-10 16:40:22 -0700

    Created Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { BankModule } from './bank/bank.module';
+import { SapModule } from './sap/sap.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, DatabaseModule, BankModule, SapModule],
   controllers: [AppController],
   providers: [AppService],
 })
